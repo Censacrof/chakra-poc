9500260c01a97d9a400d79e40bb555b416111de5
Francesco Galisi <francesco.galisi@imagicle.com>
2023-07-10 13:54:10 +0200

    Add tab and accordion

--- src/screens/Console/Queues.tsx
+++ src/screens/Console/Queues.tsx
@@ -1,5 +1,70 @@
-import { FC } from "react";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+  Tab,
+  TabList,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import { FC, ReactNode } from "react";
 
 export const Queues: FC = () => {
-  return <>queues</>;
+  return (
+    <Tabs
+      flexGrow={1}
+      alignSelf={"stretch"}
+      display={"flex"}
+      flexDirection={"column"}
+      gap={4}
+      variant={"soft-rounded"}
+      size={"sm"}
+    >
+      <TabList>
+        <Tab>Queues</Tab>
+      </TabList>
+      <TabPanels>
+        <Accordion>
+          <QueuesAccordionItem title="All queues">stuff</QueuesAccordionItem>
+
+          <QueuesAccordionItem title="Main number queue">
+            stuff
+          </QueuesAccordionItem>
+
+          <QueuesAccordionItem title="Main number queue">
+            stuff
+          </QueuesAccordionItem>
+
+          <QueuesAccordionItem title="Support queues">
+            stuff
+          </QueuesAccordionItem>
+        </Accordion>
+      </TabPanels>
+    </Tabs>
+  );
+};
+
+const QueuesAccordionItem: FC<{
+  title: string;
+  children?: ReactNode | ReactNode[];
+}> = ({ title, children }) => {
+  return (
+    <AccordionItem>
+      <h2>
+        <AccordionButton>
+          <Box as="span" flex="1" textAlign="left">
+            <Text fontSize={"sm"} fontWeight={"bold"}>
+              {title}
+            </Text>
+          </Box>
+          <AccordionIcon />
+        </AccordionButton>
+      </h2>
+      <AccordionPanel pb={4}>{children}</AccordionPanel>
+    </AccordionItem>
+  );
 };
