8ee971b9f917120d8cb8713321e5f0f1dfb26266
Francesco Galisi <francesco.galisi@imagicle.com>
2023-07-10 14:47:22 +0200

    Make StatsCard props extend CardProps

--- src/components/StatsCard.tsx
+++ src/components/StatsCard.tsx
@@ -1,11 +1,11 @@
-import { Card, CardBody, Stat } from "@chakra-ui/react";
+import { Card, CardBody, CardProps, Stat } from "@chakra-ui/react";
 import { FC, ReactNode } from "react";
 
-export const StatsCard: FC<{ children?: ReactNode | ReactNode[] }> = ({
-  children,
-}) => {
+export const StatsCard: FC<
+  { children?: ReactNode | ReactNode[] } & CardProps
+> = ({ children, ...props }) => {
   return (
-    <Card variant={"filled"}>
+    <Card variant={"filled"} {...props}>
       <CardBody>
         <Stat>{children}</Stat>
       </CardBody>
