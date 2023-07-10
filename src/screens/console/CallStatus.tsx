1bf2eb72baa64f4969f7dcda453b1c606ea418a0
Francesco Galisi <francesco.galisi@imagicle.com>
2023-07-10 14:48:16 +0200

    Add duration to CallStatus

--- src/screens/console/CallStatus.tsx
+++ src/screens/console/CallStatus.tsx
@@ -1,17 +1,35 @@
-import { Avatar, Divider, Flex, Text } from "@chakra-ui/react";
+import {
+  Avatar,
+  Divider,
+  Flex,
+  Spacer,
+  StatLabel,
+  StatNumber,
+  Text,
+} from "@chakra-ui/react";
 import { FC } from "react";
+import { StatsCard } from "../../components/StatsCard";
 
 export const CallStatus: FC = () => {
   return (
-    <Flex gap={4} height={20} alignItems={"center"}>
+    <Flex flexGrow={1} gap={4} height={20} alignItems={"center"}>
       <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
+
       <Divider orientation="vertical" />
+
       <Flex direction={"column"} alignContent={"start"}>
         <Text fontSize={"lg"} fontWeight={"bold"}>
           Dan Abrahmov
         </Text>
         <Text>237</Text>
       </Flex>
+
+      <Spacer />
+
+      <StatsCard flexGrow={0.5}>
+        <StatNumber>07:24</StatNumber>
+        <StatLabel>Avg Duration</StatLabel>
+      </StatsCard>
     </Flex>
   );
 };
