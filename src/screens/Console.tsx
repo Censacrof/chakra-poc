301ee60a3d313f388b9c548143ddc436e6a599f0
Francesco Galisi <francesco.galisi@imagicle.com>
2023-07-10 12:11:21 +0200

    Define main grid

--- src/screens/Console.tsx
+++ src/screens/Console.tsx
@@ -1,10 +1,25 @@
-import { Grid, GridItem } from "@chakra-ui/react";
-import { FC } from "react";
+import { Card, CardBody, Grid, GridItem } from "@chakra-ui/react";
+import { FC, ReactNode } from "react";
 
 export const Console: FC = () => {
   return (
-    <Grid flexGrow={1} alignSelf={"stretch"}>
-      <GridItem>asd</GridItem>
-    </Grid>
+    <Grid
+      flexGrow={1}
+      alignSelf={"stretch"}
+      templateColumns={"1fr 2fr"}
+      templateRows={"4.5rem 13.5rem 1fr"}
+    ></Grid>
+  );
+};
+
+const ConsoleGridItem: FC<{
+  children?: ReactNode | ReactNode[];
+}> = ({ children }) => {
+  return (
+    <GridItem>
+      <Card>
+        <CardBody>{children}</CardBody>
+      </Card>
+    </GridItem>
   );
 };
