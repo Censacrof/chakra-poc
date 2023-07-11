62134553b396750f09296676d574f85ccfb6356e
Francesco Galisi <francesco.galisi@imagicle.com>
2023-07-11 09:28:59 +0200

    Add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { ChakraProvider, Flex } from "@chakra-ui/react";
+import { theme } from "./chakra/theme";
 import { Console } from "./screens/console/Console";
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Flex direction={"column"} position={"fixed"} inset={0} padding={4}>
         <Console />
       </Flex>
